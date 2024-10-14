1c7f201ef9d2fc02af72ee29935bde2a1975b950
VladNedilko <52749366+VladNedilko@users.noreply.github.com>
2024-10-14 21:49:44 +0300

    Update HomeController.cs

--- StateLoggingApp/StateLoggingApp/Controllers/HomeController.cs
+++ StateLoggingApp/StateLoggingApp/Controllers/HomeController.cs
@@ -7,21 +7,17 @@ namespace StateLoggingApp.Controllers
 {
     public class HomeController : Controller
     {
-        // Обработчик GET-запроса для страницы с формой
         [HttpGet]
         public IActionResult Index()
         {
-            // Возвращаем представление с пустой моделью
             return View(new FormModel());
         }
 
-        // Обработчик POST-запроса для отправки формы
         [HttpPost]
         public IActionResult Index(FormModel model)
         {
             if (ModelState.IsValid)
             {
-                // Записываем значение из формы в Cookies
                 var cookieOptions = new CookieOptions
                 {
                     Expires = model.ExpiryDate,
@@ -29,18 +25,13 @@ namespace StateLoggingApp.Controllers
                 };
                 Response.Cookies.Append("StoredValue", model.Value, cookieOptions);
 
-                // Перенаправляем на страницу проверки Cookies
                 return RedirectToAction("CheckCookies");
             }
 
-            // В случае ошибок возвращаем форму с данными, чтобы пользователь мог их исправить
             return View(model);
         }
-
-        // Обработчик GET-запроса для страницы проверки Cookies
         public IActionResult CheckCookies()
         {
-            // Читаем значение из Cookies
             var cookieValue = Request.Cookies["StoredValue"];
             return View(new CheckCookiesModel { CookieValue = cookieValue });
         }
